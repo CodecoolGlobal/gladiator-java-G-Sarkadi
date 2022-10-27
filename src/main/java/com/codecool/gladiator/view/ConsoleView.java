589f5a860df46eb9250c71f60d945025848c9360
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-10-27 13:46:39 +0200

    Implement ConsoleView methods for input/output handling

--- src/main/java/com/codecool/gladiator/view/ConsoleView.java
+++ src/main/java/com/codecool/gladiator/view/ConsoleView.java
@@ -1,5 +1,7 @@
 package com.codecool.gladiator.view;
 
+import java.util.Scanner;
+
 /**
  * Basic console view implementation
  */
@@ -7,13 +9,22 @@ public class ConsoleView implements Viewable {
 
     @Override
     public void display(String text) {
-        // Todo
+        System.out.println(text);
     }
 
     @Override
     public int getNumberBetween(int min, int max) {
-        // Todo
-        return 2;
+        int number = - 1;
+        String text = "Please give me a number between " + min + " and " + max;
+        Scanner in = new Scanner(System.in);
+        while (number < min || number > max) {
+            display(text);
+            String input = in.nextLine();
+            try {
+                number = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+            }
+        }
+        return number;
     }
-
 }
